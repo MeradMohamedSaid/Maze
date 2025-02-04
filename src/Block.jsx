54f5f5246bd9aac67a929c829c0f2a83b89ec7e0
Said <159345605+MeradMohamedSaid@users.noreply.github.com>
2025-02-05 00:46:40 +0100

    Update Block.jsx

--- src/Block.jsx
+++ src/Block.jsx
@@ -10,100 +10,72 @@ const Block = ({
   lavaBlocks,
   sizeWidth,
   sizeHeight,
-  blocks,
   path,
   goal,
   starting,
   pathing,
 }) => {
-  const clear = "url(../public/clear.webp)";
-  const wall = "url(../public/wall.webp)";
-  const water = "url(../public/Water.webp)";
-  const sand = "url(../public/sand.webp)";
-  const lava = "url(../public/lava.gif)";
-  const pathBlock = "../public/path.webp";
-  const goalPos = "../public/goal.webp";
-  const win = "../public/win.png";
-  const steve = "../public/steve.png";
-  const flag = "../public/flag.gif";
+  // Correct image paths for the public folder
+  const clear = `url(${process.env.PUBLIC_URL}/clear.webp)`;
+  const wall = `url(${process.env.PUBLIC_URL}/wall.webp)`;
+  const water = `url(${process.env.PUBLIC_URL}/Water.webp)`;
+  const sand = `url(${process.env.PUBLIC_URL}/sand.webp)`;
+  const lava = `url(${process.env.PUBLIC_URL}/lava.gif)`;
+
+  const pathBlock = `${process.env.PUBLIC_URL}/path.webp`;
+  const goalPos = `${process.env.PUBLIC_URL}/goal.webp`;
+  const win = `${process.env.PUBLIC_URL}/win.png`;
+  const steve = `${process.env.PUBLIC_URL}/steve.png`;
+  const flag = `${process.env.PUBLIC_URL}/flag.gif`;
 
   const block = useRef();
-  const cords = { x: x, y: y };
+  const cords = { x, y };
 
   useEffect(() => {
-    if (wallBlocks.find((obj) => obj.x === x && obj.y === y)) {
+    if (wallBlocks.some((obj) => obj.x === x && obj.y === y)) {
       block.current.style.backgroundImage = wall;
+    } else if (sandBlocks.some((obj) => obj.x === x && obj.y === y)) {
+      block.current.style.backgroundImage = sand;
+    } else if (lavaBlocks.some((obj) => obj.x === x && obj.y === y)) {
+      block.current.style.backgroundImage = lava;
+    } else if (waterBlocks.some((obj) => obj.x === x && obj.y === y)) {
+      block.current.style.backgroundImage = water;
     } else {
-      if (sandBlocks.find((obj) => obj.x === x && obj.y === y)) {
-        block.current.style.backgroundImage = sand;
-      } else {
-        if (lavaBlocks.find((obj) => obj.x === x && obj.y === y)) {
-          block.current.style.backgroundImage = lava;
-        } else {
-          if (waterBlocks.find((obj) => obj.x === x && obj.y === y)) {
-            block.current.style.backgroundImage = water;
-          } else {
-            block.current.style.backgroundImage = clear;
-          }
-        }
-      }
+      block.current.style.backgroundImage = clear;
     }
-  }, [wallBlocks, sandBlocks, waterBlocks, lavaBlocks, goal]);
+  }, [wallBlocks, sandBlocks, waterBlocks, lavaBlocks, goal, x, y]);
 
   const [isStarting, setIsStarting] = useState(false);
-
   useEffect(() => {
-    if (starting.x === x && starting.y === y) {
-      setIsStarting((isStarting) => true);
-    } else {
-      setIsStarting((isStarting) => false);
-    }
-  }, [starting]);
+    setIsStarting(starting.x === x && starting.y === y);
+  }, [starting, x, y]);
 
   const [isGoal, setIsGoal] = useState(false);
   useEffect(() => {
-    if (goal.x === x && goal.y === y) {
-      setIsGoal((isGoal) => true);
-    } else {
-      setIsGoal((isGoal) => false);
-    }
-  }, [goal]);
+    setIsGoal(goal.x === x && goal.y === y);
+  }, [goal, x, y]);
 
   const [isPathing, setIsPathing] = useState(false);
   useEffect(() => {
-    if (pathing.x === x && pathing.y === y) {
-      setIsPathing((isPathing) => true);
-    } else {
-      setIsPathing((isPathing) => false);
-    }
-  }, [pathing]);
+    setIsPathing(pathing.x === x && pathing.y === y);
+  }, [pathing, x, y]);
 
   const [isPath, setIsPath] = useState(false);
   useEffect(() => {
-    if (path.find((obj) => obj.x === x && obj.y === y)) {
-      setIsPath((isPath) => true);
-    } else {
-      setIsPath((isPath) => false);
-    }
-  }, [path]);
+    setIsPath(path.some((obj) => obj.x === x && obj.y === y));
+  }, [path, x, y]);
 
   const handleBlockClick = () => {
     onClick(cords.x, cords.y);
   };
 
   return (
-    <div
-      className="mazeBlock"
-      x={x}
-      y={y}
-      ref={block}
-      onClick={handleBlockClick}
-    >
-      {isPath && !isGoal && !isStarting && <img src={pathBlock} />}
-      {isGoal && !isStarting && <img src={goalPos} />}
-      {isStarting && !isGoal && <img src={steve} />}
-      {isStarting && isGoal && <img src={win} className="bounce" />}
-      {isPathing && !isStarting && <img src={flag} />}
+    <div className="mazeBlock" ref={block} onClick={handleBlockClick}>
+      {isPath && !isGoal && !isStarting && <img src={pathBlock} alt="Path" />}
+      {isGoal && !isStarting && <img src={goalPos} alt="Goal" />}
+      {isStarting && !isGoal && <img src={steve} alt="Starting Position" />}
+      {isStarting && isGoal && <img src={win} className="bounce" alt="Win" />}
+      {isPathing && !isStarting && <img src={flag} alt="Flag" />}
     </div>
   );
 };
